a4f484083e1b02862d79f090eb150dd4e5b309f8
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-06-19 09:53:25 -0500

    Created one single database object with array of tasks and database methods so it can be exported

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -1,21 +1,30 @@
-const cardDatabase = [
-    {
-        taskName: "Test",
-        taskDescription: "Testing the task object",
-        taskDateCreated: {},
-        taskDateDue: {},
-        taskStatus: "To do"
-    }
-];
+const TaskDatabase = Object.create({}, {
+	taskArray: {
+		value: [
+			{
+				taskName: "Test",
+				taskDescription: "Testing the task object",
+				taskDateCreated: {},
+				taskDateDue: {},
+				taskStatus: "To do"
+			}
+		],
+		enumerable: true,
+		writable: true
+	},
+	save: {
+		value: (databaseObject, localStorageKey) => {
+			const stringifiedDatabase = JSON.stringify(databaseObject);
+			localStorage.setItem(localStorageKey, stringifiedDatabase);
+		}
+	},
+	load: {
+		value: localStorageKey => {
+			const databaseString = localStorage.getItem(localStorageKey);
+			return JSON.parse(databaseString);
+		}
+	}
+});
 
+module.exports = TaskDatabase;
 
-const databaseMethods = {
-    save:(databaseObject, localStorageKey) => {
-            const stringifiedDatabase = JSON.stringify(databaseObject);
-            localStorage.setItem(localStorageKey, stringifiedDatabase);
-        },
-    load:localStorageKey => {
-            const databaseString = localStorage.getItem(localStorageKey);
-            return JSON.parse(databaseString);
-        }
-};
