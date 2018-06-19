fef4676db6f9b79f667ee1d1c60335fb71da82ca
Shuaib Sajid <shusajid@gmail.com>
2018-06-19 09:54:57 -0500

    Created save.js, edits to form.js

---
+++ src/scripts/save.js
@@ -0,0 +1,11 @@
+const save = require("./database")
+const form = require("./form")
+
+//get user input
+let taskName = document.getElementById("taskName").value;
+let taskDescript = document.getElementById("taskDescript").value;
+let taskDue = document.getElementById("taskDue").value;
+ // set values
+
+
+
