1aa8ac3b8a88f59802e502f90d389bdcbde379c3
Jacob Henderson <element25668@gmail.com>
2018-06-19 11:49:12 -0500

    finished load function, tested and working

--- src/scripts/loadcards.js
+++ src/scripts/loadcards.js
@@ -2,16 +2,17 @@ const database = require("./database");
 
 const loadDatabase = () => {
    const fragment = document.createDocumentFragment();
-   const storage = database.load("key");
-   storage.forEach(item => {
+   const storage = database.load("TaskDatabase");
+//    
+       storage.taskArray.forEach(key => {
        const section = document.createElement("section");
        const youEll = document.createElement("ul");
        const ellEyeDesc = document.createElement("li");
        const ellEyeName = document.createElement("li");
        const ellEyeDate = document.createElement("li");
-       ellEyeName.textContent = `Name: ${item.taskName}`;
-       ellEyeDesc.textContent = `Description: ${item.taskDescription}`;
-       ellEyeDate.textContent = `Date Due: ${item.taskDateDue}`;
+       ellEyeName.textContent = `Name: ${key.taskName}`;
+       ellEyeDesc.textContent = `Description: ${key.taskDescription}`;
+       ellEyeDate.textContent = `Date Due: ${key.taskDateDue}`;
        youEll.appendChild(ellEyeName);
        youEll.appendChild(ellEyeDesc);
        youEll.appendChild(ellEyeDate);
